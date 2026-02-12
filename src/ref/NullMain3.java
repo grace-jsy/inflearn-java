3971e4726150f5034e3d2a4b943dd2b9272295b7
grace-jsy <sseo.oee@gmail.com>
2026-02-12 21:58:51 +0900

    study: assign new Data() to null reference

--- src/ref/NullMain3.java
+++ src/ref/NullMain3.java
@@ -4,6 +4,7 @@ public class NullMain3 {
 
     public static void main(String[] args) {
         BigData bigData = new BigData();
+        bigData.data = new Data();
         System.out.println("bigData.count = " + bigData.count);
         System.out.println("bigData.data = " + bigData.data);
 
