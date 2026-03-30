748bfed711cbb9b63a07b733eb63c3c4b0b6e6d0
grace-jsy <sseo.oee@gmail.com>
2026-03-30 18:37:58 +0900

    study: practice inheritance by extending Car class into ElectricCar and GasCar

---
+++ src/extends1/ex1/ElectricCar.java
@@ -0,0 +1,12 @@
+package extends1.ex1;
+
+public class ElectricCar {
+
+    public void move() {
+        System.out.println("A car is moving.");
+    }
+
+    public void charge() {
+        System.out.println("A car is charging.");
+    }
+}
---
+++ src/extends1/ex1/GasCar.java
@@ -0,0 +1,12 @@
+package extends1.ex1;
+
+public class GasCar {
+
+    public void move() {
+        System.out.println("A car is moving.");
+    }
+
+    public void fillUp() {
+        System.out.println("A car is filling Up.");
+    }
+}
